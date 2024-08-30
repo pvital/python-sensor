2d1a0720d5dbcdd7d8296dac127aca99991e91b1
Paulo Vital <paulo.vital@ibm.com>
2024-08-30 08:06:54 +0200

    (fix) No _stan_scope attribute error.
    Add additional checking to guarantee the attribute _stan_scope is
    present in the context object before accessing it
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/instrumentation/sqlalchemy.py
+++ src/instana/instrumentation/sqlalchemy.py
@@ -35,7 +35,7 @@ try:
             scope.span.set_tag('sqlalchemy.eng', conn.engine.name)
             scope.span.set_tag('sqlalchemy.url', url_regexp.sub('//', url))
         except Exception as e:
-            logger.debug(e)
+            logger.debug(f"sqlalchemy_inst receive_before_cursor_execute: {e}")
         return
 
 
@@ -43,29 +43,36 @@ try:
     def receive_after_cursor_execute(**kw):
         context = kw['context']
 
-        if context is not None and hasattr(context, '_stan_scope'):
+        if context and hasattr(context, "_stan_scope"):
             scope = context._stan_scope
-            if scope is not None:
+            if scope:
                 scope.close()
 
 
-    error_event = "handle_error"
     # Handle dbapi_error event; deprecated since version 0.9
-    if sqlalchemy.__version__[0] == "0":
-        error_event = "dbapi_error"
-
+    error_event = "handle_error" if sqlalchemy.__version__[0] >= "1" else "dbapi_error"
 
     def _set_error_tags(context, exception_string, scope_string):
         scope, context_exception = None, None
-        if attrgetter(scope_string)(context) and attrgetter(exception_string)(context):
-            scope = attrgetter(scope_string)(context)
-            context_exception = attrgetter(exception_string)(context)
-        if scope and context_exception:
-            scope.span.log_exception(context_exception)
+
+        try: 
+            if error_event == "dbapi_error":
+                ctx_stan_scope = context
+            else:
+                ctx_stan_scope = attrgetter(scope_string.split(".")[0])(context)
+
+            if hasattr(ctx_stan_scope, "_stan_scope") and hasattr(context, exception_string):
+                scope = attrgetter(scope_string)(context)
+                context_exception = attrgetter(exception_string)(context)
+
+            if scope and context_exception:
+                scope.span.log_exception(context_exception)
+            else:
+                scope.span.log_exception("No %s specified." % error_event)
+
             scope.close()
-        else:
-            scope.span.log_exception("No %s specified." % error_event)
-            scope.close()
+        except Exception as e:
+            logger.debug(f"sqlalchemy_inst _set_error_tags: {e}")
 
 
     @event.listens_for(Engine, error_event, named=True)
